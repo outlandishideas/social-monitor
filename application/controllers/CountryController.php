90ebfaa769251c6ccd148009852d5cefe3b6f8e1
Rasmus <rasmus.winter@gmail.com>
2013-05-20 20:14:51 +0100

    Fixing spelling mistake; Simplifying jquery slide code; Standardising view content; Styling presence titles

--- application/controllers/CountryController.php
+++ application/controllers/CountryController.php
@@ -34,7 +34,7 @@ class CountryController extends CampaignController {
         $this->view->metricOptions = self::graphMetrics();
         $this->view->tableMetrics = self::tableMetrics();
         $this->view->compareData = $compareData;
-		$this->view->title = '<span class="icon-globe"></span> '. $country->display_name . ' <a href="#" class="accordian-btn" data-id="title-info"><span class="icon-caret-down icon-small"></span></a>';
+		$this->view->title = '<span class="icon-globe"></span> '. $country->display_name . ' <a href="#" class="accordion-btn" data-id="title-info"><span class="icon-caret-down icon-small"></span></a>';
 		$this->view->titleInfo = $country->countryInfo();
         $this->view->country = $country;
 	}
