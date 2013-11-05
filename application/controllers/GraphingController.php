71b1daecf310c472ac2b18923dbf578f2bdc426d
Matt Kendon <matt@outlandishideas.co.uk>
2013-11-05 14:53:53 +0000

    added function to return code to generate a table header

--- application/controllers/GraphingController.php
+++ application/controllers/GraphingController.php
@@ -124,4 +124,144 @@ abstract class GraphingController extends BaseController {
 		}
 	}
 
+    public static function tableHeader($type, $csv = true){
+        $return = null;
+        $metrics = self::tableMetrics();
+        switch($type){
+            case('name'):
+                $return = (object)array(
+                    'name' => 'name',
+                    'sort' => 'auto',
+                    'title' => 'Name',
+                    'desc' => 'This is a description for name'
+                );
+                break;
+            case('country'):
+                $return = (object)array(
+                    'name' => 'country',
+                    'sort' => 'auto',
+                    'title' => 'Country',
+                    'desc' => 'This is a description for country'
+                );
+                break;
+            case('total-rank'):
+                $return = (object)array(
+                    'name' => 'total-rank',
+                    'sort' => 'numeric',
+                    'title' => 'Global Rank',
+                    'desc' => 'This is a description for total rank'
+                );
+                break;
+            case('total-score'):
+                $return = (object)array(
+                    'name' => 'total-score',
+                    'sort' => 'numeric',
+                    'title' => 'Overall Score',
+                    'desc' => 'This is a description for total score'
+                );
+                break;
+            case('current-audience'):
+                $return = (object)array(
+                    'name' => 'current-audience',
+                    'sort' => 'fuzzy-numeric',
+                    'title' => 'Audience',
+                    'desc' => 'This is a description for current audience'
+                );
+                break;
+            case('target-audience'):
+                $return = (object)array(
+                    'name' => 'target-audience',
+                    'sort' => 'fuzzy-numeric',
+                    'title' => 'Target Audience',
+                    'desc' => 'This is a description for target audience'
+                );
+                break;
+            case(Model_Presence::METRIC_POPULARITY_PERCENT):
+                $return = (object)array(
+                    'name' => Model_Presence::METRIC_POPULARITY_PERCENT,
+                    'sort' => 'traffic-light',
+                    'width' => '150px',
+                    'title' => $metrics[Model_Presence::METRIC_POPULARITY_PERCENT],
+                    'desc' => 'This is a description for popularity percent'
+                );
+                break;
+            case(Model_Presence::METRIC_POPULARITY_TIME):
+                $return = (object)array(
+                    'name' => Model_Presence::METRIC_POPULARITY_TIME,
+                    'sort' => 'traffic-light',
+                    'width' => '150px',
+                    'title' => $metrics[Model_Presence::METRIC_POPULARITY_TIME],
+                    'desc' => 'This is a description for popularity time'
+                );
+                break;
+            case(Model_Presence::METRIC_POSTS_PER_DAY):
+                $return = (object)array(
+                    'name' => Model_Presence::METRIC_POSTS_PER_DAY,
+                    'sort' => 'traffic-light',
+                    'width' => '150px',
+                    'title' => $metrics[Model_Presence::METRIC_POSTS_PER_DAY],
+                    'desc' => 'This is a description for actions per day'
+                );
+                break;
+            case(Model_Presence::METRIC_RESPONSE_TIME):
+                $return = (object)array(
+                    'name' => Model_Presence::METRIC_RESPONSE_TIME,
+                    'sort' => 'traffic-light',
+                    'width' => '150px',
+                    'title' => $metrics[Model_Presence::METRIC_RESPONSE_TIME],
+                    'desc' => 'This is a description for response time'
+                );
+                break;
+            case('presences'):
+                $return = (object)array(
+                    'name' => 'presences',
+                    'title' => 'Presences',
+                    'desc' => 'This is a description for presences'
+                );
+                break;
+            case('options'):
+                $return = (object)array(
+                    'name' => 'options',
+                    'desc' => 'This is a description for options'
+                );
+                break;
+            case('compare'):
+                $return = (object)array(
+                    'name' => 'compare',
+                    'sort' => 'checkbox',
+                    'title' => '<span class="icon-check"></span>',
+                    'desc' => 'This is a description for compare'
+                );
+                break;
+            case('handle'):
+                $return = (object)array(
+                    'name' => 'handle',
+                    'sort' => 'auto',
+                    'title' => 'Handle',
+                    'desc' => 'This is a description for options'
+                );
+                break;
+            case('sign-off'):
+                $return = (object)array(
+                    'name' => 'sign-off',
+                    'sort' => 'data-value-numeric',
+                    'title' => 'Sign-Off',
+                    'desc' => 'This is a description for sign off'
+                );
+                break;
+            case('branding'):
+                $return = (object)array(
+                    'name' => 'branding',
+                    'sort' => 'data-value-numeric',
+                    'title' => 'Branding',
+                    'desc' => 'This is a description for sign off'
+                );
+                break;
+            default:
+                return null;
+        }
+        $return->csv = $csv;
+        return $return;
+    }
+
 }
