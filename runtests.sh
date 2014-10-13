ea580954c4030e3cd7ce2ed852a2c6dc8d7ba3a5
Ronald Mansveld <ronald@outlandish.com>
2014-10-13 16:32:03 +0100

    Updated testscript

--- runtests.sh
+++ runtests.sh
@@ -4,13 +4,18 @@ config="$1"
 runoptional="$2"
 
 if [[ ! $# == 2 ]]; then
-	echo "Usage: runtests [path to configfile] [y/n for running selenium tests]"
+	printf "Usage: runtests [path to configfile] [y/n for running selenium tests]\n"
 fi
 
-vendor/bin/phpunit --colors --configuration $1 tests/
+vendor/bin/phpunit --configuration $1 tests/
+exitcode="$?"
 
-if [ $? -eq 0 ] && [ "$runoptional" == "y" ]; then
-	vendor/bin/phpunit --colors --configuration $1 tests/SeleniumTest.php.optional
+if [ $exitcode -eq 0 ]; then
+	if [ "$runoptional" == "y" ]; then
+		printf "Start running Selenium tests.\n"
+		vendor/bin/phpunit --colors --configuration $1 tests/SeleniumTest.php.optional
+		exitcode="$?"
+	fi
 fi
 
-exit $?
+exit $exitcode
