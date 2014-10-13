13e5cfe618a306dc36e0e53b9f169f65ee9ef539
Ronald Mansveld <ronald@outlandish.com>
2014-10-13 16:14:38 +0100

    Updated testscript + made selenium tests optional

--- runtests.sh
+++ runtests.sh
@@ -1,1 +1,16 @@
-vendor/bin/phpunit --colors --configuration tests/phpunit-ronald.xml tests/
+#!/bin/bash
+
+config="$1"
+runoptional="$2"
+
+if [[ ! $# == 2 ]]; then
+	echo "Usage: runtests [path to configfile] [y/n for running selenium tests]"
+fi
+
+vendor/bin/phpunit --colors --configuration $1 tests/
+
+if [ $? -eq 0 ] && [ "$runoptional" == "y" ]; then
+	vendor/bin/phpunit --colors --configuration $1 tests/SeleniumTest.php.optional
+fi
+
+exit $?
