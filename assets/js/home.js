c8e40e93b5f3ff5e22c791c0557d4e6507e29a8e
Rasmus <rasmus.winter@gmail.com>
2014-11-04 10:21:45 +0000

    More optimisation

--- assets/js/home.js
+++ assets/js/home.js
@@ -273,19 +273,28 @@ app.home = {
 			$el.data('score', Math.round(score));
 			var color = colorArgs.colors[0];
 			for (var j=0; j<colorArgs.colors.length-1; j++) {
-				if (score >= colorArgs.range[j] && score <= colorArgs.range[j+1]) {
+				if (score > colorArgs.range[j] && score <= colorArgs.range[j+1]) {
 					var lowColor = colorArgs.colors[j];
 					var highColor = colorArgs.colors[j+1];
-					var low = [lowColor.substring(1,3), lowColor.substring(3,5), lowColor.substring(5,7)];
-					var high = [highColor.substring(1,3), highColor.substring(3,5), highColor.substring(5,7)];
-					low = low.map(function(e) {return parseInt(e, 16);});
-					high = high.map(function(e) {return parseInt(e, 16);});
-					color = [
-						Math.floor(low[0] + ((score/100) * (high[0] - low[0]))),
-						Math.floor(low[1] + ((score/100) * (high[1] - low[1]))),
-						Math.floor(low[2] + ((score/100) * (high[2] - low[2])))
-					];
-					color = '#'+(color[0].toString(16))+(color[1].toString(16))+(color[2].toString(16));
+					if (score == colorArgs.range[j]) {
+						// score equals lower bound
+						color = colorArgs.colors[j];
+					} else if (score == colorArgs.range[j+1]) {
+						// score equals upper bound
+						color = colorArgs.colors[j+1];
+					} else {
+						// score somewhere in the middle, so interpolate the colours
+						var low = [lowColor.substring(1,3), lowColor.substring(3,5), lowColor.substring(5,7)];
+						var high = [highColor.substring(1,3), highColor.substring(3,5), highColor.substring(5,7)];
+						low = low.map(function(e) {return parseInt(e, 16);});
+						high = high.map(function(e) {return parseInt(e, 16);});
+						color = [
+							Math.floor(low[0] + ((score/100) * (high[0] - low[0]))),
+							Math.floor(low[1] + ((score/100) * (high[1] - low[1]))),
+							Math.floor(low[2] + ((score/100) * (high[2] - low[2])))
+						];
+						color = '#'+(color[0].toString(16))+(color[1].toString(16))+(color[2].toString(16));
+					}
 					break;
 				}
 			}
