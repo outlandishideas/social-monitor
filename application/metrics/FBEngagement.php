13c1bb1b7424486706741330068c832927e522b0
Ronald Mansveld <ronald@outlandish.com>
2014-12-04 12:16:16 +0000

    Moar debugging!

--- application/metrics/FBEngagement.php
+++ application/metrics/FBEngagement.php
@@ -22,12 +22,16 @@ class Metric_FBEngagement extends Metric_Abstract {
     public function calculate(Model_Presence $presence, \DateTime $start, \DateTime $end)
     {
         $data = $presence->getHistoricData($start, $end, self::getName());
+        var_dump($data);
         $total = 0;
         $count = 0;
         foreach ($data as $d) {
             $total += $d['value'];
             $count++;
         }
+        var_dump($total);
+        var_dump($count);
+        exit;
         if ($count == 0) {
             return 0;
         }
