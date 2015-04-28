25eb2fad57a42e55658ac4b4b61f3a5ceef2ba9d
Matthew Kendon <mkendon@gmail.com>
2015-04-28 13:57:53 +0100

    BCSM-141 Add Twitter and Facebook icons on graph keys Return the presence sign in the badge charts

--- application/charts/Badge.php
+++ application/charts/Badge.php
@@ -23,7 +23,7 @@ abstract class Chart_Badge extends Chart_Compare {
             $presences = Model_PresenceFactory::getPresencesById($presenceIds);
 
             foreach ($presences as $p) {
-                $names[$p->getId()] = "[{$p->getType()}]" . $p->getName();
+                $names[$p->getId()] = "[{$p->getType()->getSign()}]" . $p->getName();
             }
         }
         return $names;
