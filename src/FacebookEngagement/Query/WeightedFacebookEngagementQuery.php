e12417e1a3e80f8e8b7e81bb21f3f0b9e4af28c4
Matthew <mkendon@gmail.com>
2015-05-01 11:02:02 +0100

    Added standard and weighted queries

---
+++ src/FacebookEngagement/Query/WeightedFacebookEngagementQuery.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Matthew
+ * Date: 30/04/2015
+ * Time: 13:57
+ */
+
+namespace Outlandish\SocialMonitor\FacebookEngagement\Query;
+
+
+use DateTime;
+use PDO;
+
+class WeightedFacebookEngagementQuery implements Query
+{
+    const POPULARITY = 'popularity';
+    const FACEBOOK_STREAM_TABLE = 'facebook_stream';
+    const PRESENCE_STREAM_TABLE = 'presence_history';
+    /**
+     * @var
+     */
+    private $db;
+
+    public function __construct(PDO $db)
+    {
+        $this->db = $db;
+    }
+
+    /**
+     * @param DateTime $now
+     * @param DateTime $then
+     *
+     * @return array|null
+     */
+    public function fetch(DateTime $now, DateTime $then)
+    {
+        $popularity = self::POPULARITY;
+        $facebookStream = self::FACEBOOK_STREAM_TABLE;
+        $presenceHistory = self::PRESENCE_STREAM_TABLE;
+
+        $sql = "SELECT
+                    f.presence_id,
+                    (((SUM(f.comments)*4 + SUM(f.likes)*1 + SUM(f.share_count)*7) / MAX(ph.popularity))*100) AS `total`
+                FROM
+                    $facebookStream AS f
+                LEFT JOIN
+                    (
+                        SELECT
+                            presence_id,
+                            `value` AS popularity
+                        FROM
+                            $presenceHistory
+                        WHERE
+                            DATE(datetime) = :now
+                        AND
+                            `type` = '$popularity'
+                    ) AS ph ON f.presence_id = ph.presence_id
+                WHERE
+                    DATE(f.created_time) >= :then
+                GROUP BY
+                    presence_id";
+
+        $statement = $this->db->prepare($sql);
+        $statement->execute([
+            ':now' => $now->format('Y-m-d'),
+            ':then' => $then->format('Y-m-d')
+        ]);
+        return $statement->fetchAll(PDO::FETCH_KEY_PAIR);
+    }
+}
