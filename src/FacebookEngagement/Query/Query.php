897a4a1b4bce3c011096a8d0dde0661535bded0e
Matthew <mkendon@gmail.com>
2015-05-01 11:01:28 +0100

    Added FacebookEngagement Query Interface

---
+++ src/FacebookEngagement/Query/Query.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Matthew
+ * Date: 30/04/2015
+ * Time: 13:57
+ */
+
+namespace Outlandish\SocialMonitor\FacebookEngagement\Query;
+
+use DateTime;
+
+interface Query {
+
+    public function fetch(DateTime $now, DateTime $then);
+
+}
