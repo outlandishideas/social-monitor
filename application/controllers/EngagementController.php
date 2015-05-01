15137916a99d686e26f030dfbd39244170724079
Matthew <mkendon@gmail.com>
2015-05-01 11:04:51 +0100

    Added page for show facebook  engagement

---
+++ application/controllers/EngagementController.php
@@ -0,0 +1,13 @@
+<?php
+
+
+class EngagementController extends BaseController {
+	/**
+	 * Lists the values for different engagement scoring
+	 * @user-level manager
+	 */
+	function indexAction()
+    {
+
+	}
+}
