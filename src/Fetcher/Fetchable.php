5dd76a20fd4b78764458a67eb7f4969d6268f80d
Matthew Kendon <mkendon@gmail.com>
2015-05-04 12:58:59 +0100

    BCSM-143 Engagement review Added services.yml and added services for facebook_engagement services

---
+++ src/Fetcher/Fetchable.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: outlander
+ * Date: 01/05/2015
+ * Time: 18:05
+ */
+
+namespace Outlandish\SocialMonitor\Fetcher;
+
+
+interface Fetchable
+{
+    public function getFetchableId();
+}
