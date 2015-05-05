f0224beb1f210cb2974cb492946def107c3b9e50
Matthew Kendon <mkendon@gmail.com>
2015-05-05 10:23:00 +0100

    BCSM-143 Engagement review Added new ResponseTime metric that removes the min and max response time values before calculating a score.

---
+++ application/metrics/ResponseTimeNew.php
@@ -0,0 +1,38 @@
+<?php
+
+class Metric_ResponseTimeNew extends Metric_ResponseTime {
+
+    protected static $name = "response_time";
+    protected static $title = "Responsiveness";
+    protected static $icon = "fa fa-clock-o";
+
+    /**
+     * Counts the months between now and estimated date of reaching target audience
+     * calculates score based on
+     * @param Model_Presence $presence
+     * @param DateTime $start
+     * @param DateTime $end
+     * @return null/string
+     */
+    public function calculate(Model_Presence $presence, \DateTime $start, \DateTime $end)
+    {
+        $data = $presence->getResponseData($start, $end);
+        if (is_null($data)) return null;
+        if (!$data || empty($data)) return 0;
+
+        $diffs = array_map(function($row) {
+            return $row->diff;
+        }, $data);
+
+        $total = array_sum($diffs);
+
+        //if we have less than 5 values, don't remove min and max values
+        //else get min and max values and remove from the total
+        if ($diffs >= 5) {
+            $total -= min($diffs);
+            $total -= max($diffs);
+        }
+
+        return $total/count($diffs);
+    }
+}
