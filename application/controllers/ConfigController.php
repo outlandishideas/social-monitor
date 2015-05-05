bdaaf7df1553c549d7de4ee23476c81d8bbfb993
Matthew Kendon <mkendon@gmail.com>
2015-05-05 09:48:43 +0100

    BCSM-143 Engagement review Added 5 levels to facebook engagement in config screen

--- application/controllers/ConfigController.php
+++ application/controllers/ConfigController.php
@@ -93,7 +93,11 @@ class ConfigController extends BaseController {
                         'description' => 'The Facebook Engagement Score is based on Social Baker\'s Daily Page Engagement Rate calculation.',
                         'values' => array(
                             'fb_engagement_score_weighting'=>array('label'=>'Facebook Engagement Score Weighting'),
-                            'fb_engagement_target'=>array('label'=>'Facebook Engagement Score Target', 'hint'=>'The presence will score 100% if it meets or exceeds this target, but will only receive 0% if it does not meet it.')
+                            'fb_engagement_target_level_1'=>array('label'=>'Facebook Engagement Score Level 1', 'hint' => 'The presence will score 20% if it reaches this target'),
+                            'fb_engagement_target_level_2'=>array('label'=>'Facebook Engagement Score Level 2', 'hint' => 'The presence will score 40% if it reaches this target'),
+                            'fb_engagement_target_level_3'=>array('label'=>'Facebook Engagement Score Level 3', 'hint' => 'The presence will score 60% if it reaches this target'),
+                            'fb_engagement_target_level_4'=>array('label'=>'Facebook Engagement Score Level 4', 'hint' => 'The presence will score 80% if it reaches this target'),
+                            'fb_engagement_target_level_5'=>array('label'=>'Facebook Engagement Score Level 5', 'hint' => 'The presence will score 100% if it reaches this target')
                         )
                     ),
                 )
