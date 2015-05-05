a72999b830e233c33ebc59c80bc97323aaad686d
Matthew Kendon <mkendon@gmail.com>
2015-05-05 10:26:52 +0100

    BCSM-143 Engagement review Moved new Facebook Engagement metric calculations to new class and updated old class to use the original calculations and new 100% target

--- application/metrics/FBEngagement.php
+++ application/metrics/FBEngagement.php
@@ -9,19 +9,7 @@ class Metric_FBEngagement extends Metric_Abstract {
 
     function __construct()
     {
-        $this->target = $this->getTargets();
-    }
-
-    private function getTargets()
-    {
-        $target = [];
-        $target[1] = floatval(BaseController::getOption('fb_engagement_target_level_1'));
-        $target[2] = floatval(BaseController::getOption('fb_engagement_target_level_2'));
-        $target[3] = floatval(BaseController::getOption('fb_engagement_target_level_3'));
-        $target[4] = floatval(BaseController::getOption('fb_engagement_target_level_4'));
-        $target[5] = floatval(BaseController::getOption('fb_engagement_target_level_5'));
-
-        return $target;
+        $this->target = floatval(BaseController::getOption('fb_engagement_target_level_5'));
     }
 
     /**
@@ -49,29 +37,8 @@ class Metric_FBEngagement extends Metric_Abstract {
     public function getScore(Model_Presence $presence, \DateTime $start, \DateTime $end)
     {
         $score = $presence->getMetricValue($this);
-
-        foreach($this->target as $level => $target) {
-            //if we haven't reached level 1 yet, then
-            //set the level as 0 and break out
-            if ($level == 1 && $score < $target) {
-                $level = 0;
-                break;
-            }
-
-            //if level is more than or == to the target
-            //continue to the next level
-            if ($level > $target) {
-                continue;
-            }
-
-            //if level is less than or = to the target
-            //break and use the current $level to calculate
-            break;
-
-        }
-
-        //Each level is worth 20% so level 5 is 100%
-        return $level * 20;
+        $score = ($score < $this->target) ? 0 : 100 ;
+        return $score;
     }
 
 }
---
+++ application/metrics/FBEngagementLeveled.php
@@ -0,0 +1,77 @@
+<?php
+
+class Metric_FBEngagementLeveled extends Metric_FBEngagement {
+
+    protected static $name = "facebook_engagement";
+    protected static $title = "Facebook Engagement Score";
+    protected static $icon = "fa fa-facebook-square";
+    protected static $gliding = false;
+
+    function __construct()
+    {
+        $this->target = $this->getTargets();
+    }
+
+    private function getTargets()
+    {
+        $target = [];
+        $target[1] = floatval(BaseController::getOption('fb_engagement_target_level_1'));
+        $target[2] = floatval(BaseController::getOption('fb_engagement_target_level_2'));
+        $target[3] = floatval(BaseController::getOption('fb_engagement_target_level_3'));
+        $target[4] = floatval(BaseController::getOption('fb_engagement_target_level_4'));
+        $target[5] = floatval(BaseController::getOption('fb_engagement_target_level_5'));
+
+        return $target;
+    }
+
+    /**
+     * Gets the average facebook engagement over the given range
+     * @param Model_Presence $presence
+     * @param DateTime $start
+     * @param DateTime $end
+     * @return int
+     */
+    public function calculate(Model_Presence $presence, \DateTime $start, \DateTime $end)
+    {
+        $data = $presence->getHistoricData($start, $end, self::getName());
+        $total = 0;
+        $count = 0;
+        foreach ($data as $d) {
+            $total += $d['value'];
+            $count++;
+        }
+        if ($count == 0) {
+            return 0;
+        }
+        return $total/$count;
+    }
+
+    public function getScore(Model_Presence $presence, \DateTime $start, \DateTime $end)
+    {
+        $score = $presence->getMetricValue($this);
+
+        foreach($this->target as $level => $target) {
+            //if we haven't reached level 1 yet, then
+            //set the level as 0 and break out
+            if ($level == 1 && $score < $target) {
+                $level = 0;
+                break;
+            }
+
+            //if level is more than or == to the target
+            //continue to the next level
+            if ($level > $target) {
+                continue;
+            }
+
+            //if level is less than or = to the target
+            //break and use the current $level to calculate
+            break;
+
+        }
+
+        //Each level is worth 20% so level 5 is 100%
+        return $level * 20;
+    }
+
+}
