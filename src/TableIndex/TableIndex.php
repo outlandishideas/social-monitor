e3f5bcd29d4fb6cb7a2d9cf37993503a54f45f93
Matthew <mkendon@gmail.com>
2015-06-10 13:27:29 +0100

    Added TableIndex class to handle generating row data from array of Models and Table Headers

---
+++ src/TableIndex/TableIndex.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Matthew
+ * Date: 10/06/2015
+ * Time: 11:22
+ */
+
+namespace Outlandish\SocialMonitor\TableIndex;
+
+
+use Outlandish\SocialMonitor\TableIndex\Header\Header;
+
+class TableIndex {
+
+    /**
+     * @var \Header_Abstract[]
+     */
+    private $headers = array();
+
+    public function addHeader(Header $header)
+    {
+        $this->headers[] = $header;
+    }
+
+    public function getHeaders()
+    {
+        return $this->headers;
+    }
+
+    public function getRows(array $data)
+    {
+        $rows = [];
+
+        foreach ($data as $datum) {
+            $row = [
+                'id' => $datum->id
+            ];
+            foreach ($this->headers as $header) {
+                $row[$header->getName()] = $header->getTableCellValue($datum);
+            }
+            $rows[] = (object)$row;
+        }
+
+        return $rows;
+    }
+
+}
