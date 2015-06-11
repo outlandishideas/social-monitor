8c57e8977b3dd53d56a8bb9cc36f1d065a876a37
Matthew <mkendon@gmail.com>
2015-06-11 12:16:00 +0100

    Removed the responsivness and replies metrics from the description

--- application/badges/Engagement.php
+++ application/badges/Engagement.php
@@ -6,8 +6,6 @@ class Badge_Engagement extends Badge_Abstract
 	protected static $title = 'Engagement';
 	protected static $description = '<p>The Engagement KPI provides an overall score for how well the presence, country or SBU engages with its audience. This score combines the following metrics:</p>
 						<ul>
-							<li>The ratio of replies by the presence owner to the number of posts / tweets by others.</li>
-							<li>The average time it takes to reply to a post / tweet.</li>
 							<li>The Klout Score for this presence (Twitter Only).</li>
 							<li>The Facebook Engagement score for this presence (Facebook Only).</li>
 						</ul>';
