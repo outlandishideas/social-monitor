2c5b482bdff3773c12b06589fd1f32b58ad6b5b4
Joaquim d'Souza <joaquimds@gmail.com>
2015-10-28 14:24:42 +0000

    w.i.p.

---
+++ src/Adapter/InstagramAdapter.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Outlandish\SocialMonitor\Adapter;
+
+class InstagramAdapter extends AbstractAdapter
+{
+
+    public function getMetadata($handle) {
+
+    }
+
+    public function getStatuses($pageUID, $since, $handle) {
+
+    }
+
+    public function getResponses($postUIDs) {
+
+    }
+}
---
+++ src/Models/InstagramStatus.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Outlandish\SocialMonitor\Models;
+
+class InstagramStatus {
+
+    public $image_url;
+
+}
