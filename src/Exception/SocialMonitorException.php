ac5ad30cb42cfea494f6b0370108d0da932e9921
Matthew <mkendon@gmail.com>
2015-11-26 15:51:47 +0000

    added exception specific for Social monitor project

---
+++ src/Exception/SocialMonitorException.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Outlandish\SocialMonitor\Exception;
+
+/**
+ * Base exception class for all exceptions thrown by the Social Monitor project
+ *
+ * @package Outlandish\SocialMonitor\Exception
+ */
+class SocialMonitorException extends \Exception
+{
+
+}
