5b9dd3a4b73ad5c9e64cb5ec66dd3b52f167bacf
Joaquim d'Souza <joaquimds@gmail.com>
2015-12-17 14:56:32 +0000

    paginate results from youtube api

--- src/Adapter/YoutubeAdapter.php
+++ src/Adapter/YoutubeAdapter.php
@@ -52,29 +52,45 @@ class YoutubeAdapter extends AbstractAdapter
         $videos = array();
         $channels = $this->youtube->channels->listChannels('contentDetails',['forUsername' => $handle])->getItems();
         if($channels && count($channels)) {
+            $media = array();
             $playlistId = $channels[0]->contentDetails->relatedPlaylists->uploads;
-            $media = $this->youtube->playlistItems->listPlaylistItems('snippet', ['playlistId' => $playlistId])->getItems();
-            $videoIds = array();
-            foreach($media as $m) {
-                if($m->snippet->resourceId->kind === 'youtube#video') {
-                    $videoIds[] = $m->snippet->resourceId->videoId;
+            $args = ['playlistId' => $playlistId, 'maxResults' => 50];
+            $complete = false;
+            while(!$complete) {
+                $playlistItemResponse = $this->youtube->playlistItems
+                    ->listPlaylistItems('snippet',$args);
+                $playlistItems = $playlistItemResponse->getItems();
+
+                $videoIds = array();
+                foreach ($playlistItems as $p) {
+                    if ($p->snippet->resourceId->kind === 'youtube#video') {
+                        $videoIds[] = $p->snippet->resourceId->videoId;
+                    }
+                }
+
+                $q = ['id'=>implode(',',$videoIds)];
+                $details = $this->youtube->videos->listVideos('snippet,statistics', $q)->getItems();
+                $media = array_merge($media,$details);
+
+                if(!$playlistItemResponse->nextPageToken) {
+                    $complete = true;
+                } else {
+                    $args['pageToken'] = $playlistItemResponse->nextPageToken;
                 }
             }
-            $q = ['id'=>implode(',',$videoIds)];
-            $details = $this->youtube->videos->listVideos('snippet,statistics', $q)->getItems();
 
-            foreach ($details as $m) {
+            foreach ($media as $v) {
                 $video = new YoutubeVideo();
-                $video->id = $m->id;
-                $video->comments = $m->statistics->commentCount ? $m->statistics->commentCount : 0;
-                $video->likes = $m->statistics->likeCount ? $m->statistics->likeCount : 0;
-                $video->dislikes = $m->statistics->dislikeCount ? $m->statistics->dislikeCount : 0;
-                $video->views = $m->statistics->viewCount ? $m->statistics->viewCount : 0;
-                $video->created_time = strtotime($m->snippet->publishedAt);
+                $video->id = $v->id;
+                $video->comments = $v->statistics->commentCount ? $v->statistics->commentCount : 0;
+                $video->likes = $v->statistics->likeCount ? $v->statistics->likeCount : 0;
+                $video->dislikes = $v->statistics->dislikeCount ? $v->statistics->dislikeCount : 0;
+                $video->views = $v->statistics->viewCount ? $v->statistics->viewCount : 0;
+                $video->created_time = strtotime($v->snippet->publishedAt);
                 $video->posted_by_owner = true;
                 $video->permalink = 'https://www.youtube.com/watch?v=' . $video->id;
-                $video->title = $m->snippet->title;
-                $video->description = $m->snippet->description;
+                $video->title = $v->snippet->title;
+                $video->description = $v->snippet->description;
 
                 $videos[] = $video;
             }
