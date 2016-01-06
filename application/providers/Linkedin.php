64ad9c4fb7dbd81f20683c7deb3ef4aaf098b58a
Matthew <mkendon@gmail.com>
2016-01-06 17:56:05 +0000

    cleaned up provider and fixed the getHistory method

--- application/providers/Linkedin.php
+++ application/providers/Linkedin.php
@@ -38,47 +38,6 @@ class Provider_Linkedin extends Provider_Abstract
         return $count;
     }
 
-    /**
-     *
-     * Store a snapshot of the YoutubeVideo data, so we can track how views, likes, dislikes and comments change over
-     * time.
-     *
-     * @param Model_Presence $presence
-     */
-    private function updateVideoHistory($presence)
-    {
-        $query = $this->db->prepare("SELECT * FROM `youtube_video_stream` WHERE `presence_id`=" . $presence->getId());
-        $query->execute();
-        $result = $query->fetchAll(PDO::FETCH_ASSOC);
-
-        $insertStmt = $this->db->prepare("
-        	INSERT INTO `youtube_video_history`
-        	(`video_id`, `datetime`, `type`, `value`)
-        	VALUES
-        	(:id, :datetime, :type, :value)
-        	ON DUPLICATE KEY UPDATE
-        	`value` = VALUES(`value`)
-        ");
-        $date = gmdate('Y-m-d H:i:s');
-
-        foreach($result as $row) {
-            foreach ($this->videoHistoryColumns as $type) {
-                $value = $row[$type];
-                if (!is_null($value)) {
-                    $result = $insertStmt->execute(array(
-                        ':id' => $row['id'],
-                        ':datetime' => $date,
-                        ':type' => $type,
-                        ':value' => $value
-                    ));
-                    if(!$result) {
-                        $error = $insertStmt->errorInfo();
-                        error_log('Error saving youtube video history: '.$error[2]);
-                    }
-                }
-            }
-        }
-    }
 
 	public function getHistoricStream(Model_Presence $presence, \DateTime $start, \DateTime $end,
         $search = null, $order = null, $limit = null, $offset = null)
@@ -93,7 +52,7 @@ class Provider_Linkedin extends Provider_Abstract
             ':end'   => $end->format('Y-m-d H:i:s'),
             ':id'    => $presence->getId()
         );
-        $searchArgs = $this->getSearchClauses($search, array('p.title','p.description'));
+        $searchArgs = $this->getSearchClauses($search, array('p.message'));
         $clauses = array_merge($clauses, $searchArgs['clauses']);
         $args = array_merge($args, $searchArgs['args']);
 
@@ -151,18 +110,6 @@ class Provider_Linkedin extends Provider_Abstract
 		parent::update($presence);
     }
 
-    public function calculateYoutubeEngagement(Model_Presence $presence)
-    {
-        $now = new DateTime();
-        $then = clone $now;
-        $then->modify("-1 week");
-
-        $query = new Outlandish\SocialMonitor\Engagement\Query\WeightedYoutubeEngagementQuery($this->db);
-        $metric = new Outlandish\SocialMonitor\Engagement\EngagementMetric($query);
-
-        return $metric->get($presence->getId(), $now, $then);
-    }
-
     public function updateMetadata(Model_Presence $presence) {
 
         try {
@@ -179,92 +126,6 @@ class Provider_Linkedin extends Provider_Abstract
         $presence->popularity = $metadata->popularity;
         $presence->image_url = $metadata->image_url;
 	}
-
-    /**
-     * @param Model_Presence $presence
-     * @param DateTime $start
-     * @param DateTime $end
-     * @return array
-     */
-    public function getResponseData(Model_Presence $presence, DateTime $start, DateTime $end)
-    {
-        $responseData = array();
-        /*$clauses = array(
-            'r.presence_id = :pid',
-            't.created_time >= :start_date',
-            't.created_time <= :end_date'
-        );
-        $args = array(
-            ':pid'=>$presence->getId(),
-            ':start_date' => $start->format('Y-m-d'),
-            ':end_date' => $end->format('Y-m-d')
-        );
-        $stmt = $this->db->prepare("
-          SELECT t.post_id as id, t.created_time as created, TIME_TO_SEC( TIMEDIFF( r.created_time, t.created_time ))/3600 AS time
-          FROM {$this->tableName} AS t
-            INNER JOIN {$this->tableName} AS r ON t.post_id = r.in_response_to
-            WHERE " . implode(' AND ', $clauses) ."");
-        $stmt->execute($args);
-        foreach ($stmt->fetchAll(PDO::FETCH_OBJ) as $r) {
-            $key = $r->id;
-            if(!array_key_exists($key, $responseData)) {
-                $responseData[$key] = (object)array('diff' => null, 'created' => null);
-            }
-            if (empty($responseData[$key]->diff) || $r->time < $responseData[$key]->diff) {
-                $responseData[$key]->diff = $r->time;
-                $responseData[$key]->created = $r->created;
-            }
-        }*/
-        return $responseData;
-    }
-
-    /**
-     * get ids of responses that need to be updateable
-     *
-     * @param Model_Presence $presence
-     * @return array
-     */
-    protected function getUpdateableResponses(Model_Presence $presence)
-    {
-        $presenceId = $presence->getId();
-
-        // update the responses for any non-page posts that don't have a response yet.
-        // Only get those that explicitly need one, or were posted in the last 3 days
-        $args = array(
-            ':id' => $presenceId,
-            ':necessary_since' => date('Y-m-d H:i:s', strtotime(date('Y-m-d H:i:s') . ' -30 days')),
-            ':unnecessary_since' => date('Y-m-d H:i:s', strtotime(date('Y-m-d H:i:s') . ' -3 days'))
-        );
-        $stmt = $this->db->prepare("SELECT DISTINCT a.post_id
-            FROM (
-                SELECT *
-                FROM {$this->tableName}
-                WHERE presence_id = :id
-                AND in_response_to IS NULL
-                AND
-                (
-                    (
-                      needs_response = 1
-                      AND created_time > :necessary_since
-                    )
-                    OR
-                    (
-                      posted_by_owner = 0
-                      AND message <> ''
-                      AND message IS NOT NULL
-                      AND created_time > :unnecessary_since
-                    )
-                )
-            ) as a
-            LEFT OUTER JOIN {$this->tableName} AS b
-                ON b.presence_id = a.presence_id
-                AND b.in_response_to = a.post_id
-            WHERE b.id IS NULL");
-        $stmt->execute($args);
-        $postIds = $stmt->fetchAll(PDO::FETCH_COLUMN);
-
-        return $postIds;
-    }
 
     /**
      * @param Model_Presence $presence
@@ -313,88 +174,6 @@ class Provider_Linkedin extends Provider_Abstract
             $count++;
         }
 
-    }
-
-    public function getStatusStream(Model_Presence $presence, $start, $end, $search, $order, $limit, $offset)
-    {
-
-        $clauses = array(
-            'p.created_time >= :start',
-            'p.created_time <= :end',
-            'p.presence_id = :id'
-        );
-        $args = array(
-            ':start' => $start->format('Y-m-d H:i:s'),
-            ':end'   => $end->format('Y-m-d H:i:s'),
-            ':id'    => $presence->getId()
-        );
-        $searchArgs = $this->getSearchClauses($search, array('p.message'));
-        $clauses = array_merge($clauses, $searchArgs['clauses']);
-        $args = array_merge($args, $searchArgs['args']);
-
-        $sql = "
-			SELECT SQL_CALC_FOUND_ROWS p.*
-			FROM {$this->commentTableName} AS p
-			WHERE " . implode(' AND ', $clauses);
-        $sql .= $this->getOrderSql($order, array('date'=>'created_time'));
-        $sql .= $this->getLimitSql($limit, $offset);
-
-        $stmt = $this->db->prepare($sql);
-        $stmt->execute($args);
-        $ret = $stmt->fetchAll(PDO::FETCH_ASSOC);
-        $total = $this->db->query('SELECT FOUND_ROWS()')->fetch(PDO::FETCH_COLUMN);
-
-        return (object)array(
-            'stream' => count($ret) ? $ret : null,
-            'total' => $total
-        );
-    }
-
-    /**
-     * @param Model_Presence $presence  the presence to fetch the data for
-     * @param DateTime $start  the date from which to fetch historic data from (inclusive)
-     * @param DateTime $end  the date from which to fetch historic data to (inclusive)
-     * @param array $types  the types of data to be returned from the history table (if empty all types will be returned)
-     * @return array
-     */
-    public function getHistoryData(Model_Presence $presence, \DateTime $start, \DateTime $end, $types = [])
-    {
-        if (empty($types)) {
-            $types = $this->videoHistoryColumns;
-        }
-
-        $sql = "SELECT id FROM youtube_video_stream WHERE `presence_id` = :presence_id";
-        $statement = $this->db->prepare($sql);
-        $result = $statement->execute([':presence_id' => $presence->getId()]);
-
-        if (!$result) {
-            return [];
-        }
-
-        $videoIds = $statement->fetchAll(PDO::FETCH_COLUMN);
-
-        $arguments = [
-            ':start_date' => $start->format("Y-m-d"),
-            ':end_date' => $end->format("Y-m-d"),
-        ];
-
-        $tableName = self::$historyTableName;
-        $sql = "SELECT `datetime`, `type`, SUM(`value`) AS `value` FROM {$tableName}
-                WHERE `video_id` IN ('" .implode("','", $videoIds) . "')
-                AND `datetime` <= :end_date
-                AND `datetime` >= :start_date
-                AND type IN ('" .implode("','", $types) . "')
-                GROUP BY `type`, `datetime`;";
-
-        $statement = $this->db->prepare($sql);
-        $result = $statement->execute($arguments);
-        if ($result) {
-            $data = $statement->fetchAll(PDO::FETCH_OBJ);
-        } else {
-            $data = [];
-        }
-
-        return $data;
     }
 
 }
