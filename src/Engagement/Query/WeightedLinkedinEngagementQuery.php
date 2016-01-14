93e5df0d019732cf12b35a3061ca3615283e29a4
Joaquim d'Souza <joaquimds@gmail.com>
2016-01-14 17:51:42 +0000

    linked in also done. all need testing tomorrow

--- src/Engagement/Query/WeightedLinkedinEngagementQuery.php
+++ src/Engagement/Query/WeightedLinkedinEngagementQuery.php
@@ -9,6 +9,7 @@
 namespace Outlandish\SocialMonitor\Engagement\Query;
 
 
+use BaseController;
 use DateTime;
 use PDO;
 
@@ -17,6 +18,8 @@ class WeightedLinkedinEngagementQuery implements Query
     const POPULARITY = 'popularity';
     const STREAM_TABLE = 'linkedin_stream';
     const PRESENCE_STREAM_TABLE = 'presence_history';
+    const PRESENCES_TABLE = 'presences';
+
     /**
      * @var
      */
@@ -25,6 +28,10 @@ class WeightedLinkedinEngagementQuery implements Query
     public function __construct(PDO $db)
     {
         $this->db = $db;
+        $this->activeUserProportion = array();
+        $this->activeUserProportion[0] = BaseController::getOption('in_active_user_percentage_small') / 100;
+        $this->activeUserProportion[1] = BaseController::getOption('in_active_user_percentage_medium') / 100;
+        $this->activeUserProportion[2] = BaseController::getOption('in_active_user_percentage_large') / 100;
     }
 
     /**
@@ -42,39 +49,49 @@ class WeightedLinkedinEngagementQuery implements Query
         $popularity = self::POPULARITY;
         $facebookStream = self::STREAM_TABLE;
         $presenceHistory = self::PRESENCE_STREAM_TABLE;
+        $presencesTable = self::PRESENCES_TABLE;
 
         $sql = "SELECT
                     f.presence_id,
+                    ph.size,
 					(((f.comments*4 + f.likes) / 5) / ph.popularity) * 1000 AS `total`
                 FROM
 				    (
 						SELECT
 							presence_id,
 							SUM(comments) AS comments,
-							SUM(likes) AS likes,
+							SUM(likes) AS likes
 						FROM
 							$facebookStream
 						WHERE
 							DATE(created_time) >= :then
 						AND
 							DATE(created_time) <= :now
-					    AND
-					        in_response_to IS NULL
 						GROUP BY
 							presence_id
 					) AS f
                 LEFT JOIN
                     (
                         SELECT
-                            presence_id,
-                            MAX(`value`) AS popularity
-                        FROM
-                            $presenceHistory
-                        WHERE
-                            DATE(datetime) = :now
-                        AND
-                            `type` = '$popularity'
-						GROUP BY presence_id
+                            p.id as presence_id,
+                            p.size as size,
+                            h.popularity as popularity
+                        FROM (SELECT
+						        id,
+						        size
+						    FROM $presencesTable) as p
+						LEFT JOIN
+                            (SELECT
+                                presence_id,
+                                MAX(`value`) AS popularity
+                            FROM
+                                $presenceHistory
+                            WHERE
+                                DATE(datetime) = :now
+                            AND
+                                `type` = '$popularity'
+						GROUP BY presence_id) AS h
+						ON p.id = h.presence_id
                     ) AS ph ON f.presence_id = ph.presence_id";
 
         $statement = $this->db->prepare($sql);
@@ -82,6 +99,13 @@ class WeightedLinkedinEngagementQuery implements Query
             ':now' => $now->format('Y-m-d'),
             ':then' => $then->format('Y-m-d')
         ]);
-        return $statement->fetchAll(PDO::FETCH_KEY_PAIR);
+        $data = $statement->fetchAll(PDO::FETCH_ASSOC);
+        $scores = array();
+        // create key => value array, scaling by the active user proportion
+        foreach($data as $d) {
+            $scale = $this->activeUserProportion[$d['size']] ? $this->activeUserProportion[$d['size']] : 1;
+            $scores[$d['presence_id']] = $d['total'] / $scale;
+        }
+        return $scores;
     }
 }
