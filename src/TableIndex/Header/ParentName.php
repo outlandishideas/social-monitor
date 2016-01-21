337c7eccd06fc96000624fa153b4a9611fa26813
Matthew <mkendon@gmail.com>
2016-01-21 15:40:22 +0000

    added new parent name column to display country/sbu for presences index page

---
+++ src/TableIndex/Header/ParentName.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Outlandish\SocialMonitor\TableIndex\Header;
+
+use Model_Presence;
+
+class ParentName extends Header {
+
+    protected static $name = "parent-name";
+
+    function __construct()
+    {
+        $this->label = "Country/SBU";
+        $this->allowedTypes = array(self::MODEL_TYPE_CAMPAIGN);
+        $this->cellClasses[] = 'left-align';
+    }
+
+
+    /**
+     * @param Model_Presence $model
+     * @return mixed
+     */
+    public function getValue($model = null)
+    {
+        if (!($model instanceof Model_Presence)) {
+            return "";
+        }
+
+        $owner = $model->getOwner();
+
+        if (!$owner) {
+            return "N/A";
+        }
+
+        return $owner->display_name;
+    }
+
+
+}
