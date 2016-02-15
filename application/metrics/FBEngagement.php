a47b85983156a117016f8351ca21ee91fa26e294
Joaquim d'Souza <joaquimds@gmail.com>
2016-02-15 17:28:53 +0000

    tweaking facebook engagement score

--- application/metrics/FBEngagement.php
+++ application/metrics/FBEngagement.php
@@ -5,7 +5,7 @@ class Metric_FBEngagement extends Metric_AbstractEngagement {
     protected static $name = "facebook_engagement";
     protected static $title = "Facebook Engagement Score";
     protected static $icon = "fa fa-facebook-square";
-    public $target = 0.25;
+    public $target = 0.15;
     protected static $gliding = false;
     protected static $queryClassName = 'Outlandish\SocialMonitor\Engagement\Query\WeightedFacebookEngagementQuery';
 
