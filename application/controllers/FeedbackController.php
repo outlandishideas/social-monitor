0168a93c96b5e1f8a9285283e050002ca0c877b6
Joaquim d'Souza <joaquimds@gmail.com>
2016-03-23 18:36:54 +0000

    FeedbackController done

--- application/controllers/FeedbackController.php
+++ application/controllers/FeedbackController.php
@@ -22,14 +22,14 @@ class FeedbackController extends BaseController {
             // Show an error if missing any parameters.
             // This is also enforced on the front end form.
             if(!$this->_request->getParam('name')) {
-                $error = 'Please enter your name.';
+                $error = $this->translator->trans('Feedback.index.error.missing-name');
                 $valid = false;
             } else if(!$this->_request->getParam('from') ||
                 !filter_var($this->_request->getParam('from') , FILTER_VALIDATE_EMAIL)) {
-                $error = 'Please enter a valid email address.';
+				$error = $this->translator->trans('Feedback.index.error.missing-email');
                 $valid = false;
             } else if(!$this->_request->getParam('body')) {
-                $error = 'Please enter your message.';
+				$error = $this->translator->trans('Feedback.index.error.missing-message');
                 $valid = false;
             }
 
@@ -38,7 +38,11 @@ class FeedbackController extends BaseController {
             $name = substr($this->_request->getParam('name'),0,64);
             $from = substr($this->_request->getParam('from'),0,64);
             $body = substr($this->_request->getParam('body'),0,1000);
-            $subject = 'British Council Social Monitor feedback from '.$name;
+            $subject = $this->config->app->client_name . ' '.str_replace(
+					'[]',
+					$name,
+					$this->translator->trans('Feedback.index.email-subject')
+				);
 
             if($valid) {
                 $success = (bool) $this->sendEmail($body, $from, $name, $this->feedbackRecipient, $subject, true);
