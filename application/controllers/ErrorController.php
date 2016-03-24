92cb154914137dd8aa970d174a83419a4557ec14
Matthew <mkendon@gmail.com>
2016-03-24 11:57:52 +0000

    translated Error Controller

--- application/controllers/ErrorController.php
+++ application/controllers/ErrorController.php
@@ -7,29 +7,29 @@ class ErrorController extends BaseController
     public function errorAction()
     {
         $errors = $this->_getParam('error_handler');
-        
+
         // $boot = $this->getInvokeArg('bootstrap');
         // if ($boot->hasResource('logger')) {
         	// $boot->getResource('logger')->err($errors->exception);
         // }
-        
-        switch ($errors->type) { 
+
+        switch ($errors->type) {
             case Zend_Controller_Plugin_ErrorHandler::EXCEPTION_NO_CONTROLLER:
             case Zend_Controller_Plugin_ErrorHandler::EXCEPTION_NO_ACTION:
-        
+
                 // 404 error -- controller or action not found
                 $this->getResponse()->setHttpResponseCode(404);
-                $this->view->pageTitle = 'Page not found';
-                $this->view->text = 'Please check the url';
+                $this->view->pageTitle = $this->translator->trans('Error.404.title'); //'Page not found';
+                $this->view->text = $this->translator->trans('Error.404.message'); //'Please check the url';
                 break;
             default:
-                // application error 
+                // application error
                 $this->getResponse()->setHttpResponseCode(500);
-                $this->view->pageTitle = 'Application error';
-                $this->view->text = 'Sorry, something has gone wrong';
+                $this->view->pageTitle = $this->translator->trans('Error.500.title'); //'Application error';
+                $this->view->text = $this->translator->trans('Error.500.message'); //'Sorry, something has gone wrong';
                 break;
         }
-        
+
         $this->view->exception = $errors->exception;
         $this->view->request   = $errors->request;
     }
