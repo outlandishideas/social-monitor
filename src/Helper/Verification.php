0c619e981534046c2dd323ed112eb3b6ce3786d9
Jannik Kollmann <kollmann_jannik@yahoo.com>
2016-04-07 16:36:29 +0100

    Introduce basic type verification

--- src/Helper/Verification.php
+++ src/Helper/Verification.php
@@ -3,6 +3,34 @@
 namespace Outlandish\SocialMonitor\Helper;
 class Verification
 {
+
+    public static $sqlTypeMappings = [
+        'int' => 'integer',
+        'tinyint' => 'integer',
+        'smallint' => 'integer',
+        'bigint' => 'integer',
+        'float' => 'double',
+        'varchar' => 'string',
+        'text' => 'string',
+        'mediumtext' => 'string',
+        'datetime' => 'date',
+        'date' => 'date',
+        'timestamp' => 'date',
+        'mediumBlob' => 'blob'
+    ];
+
+    public static function verifyType($sqlType, $value){
+        $type = self::$sqlTypeMappings[$sqlType];
+       
+        if($type === 'integer' || $type === 'double'){
+            return is_numeric($value);
+        }
+        
+        return true;
+    }
+
+
+
     public static function pluck($key, $data) {
         return array_reduce($data, function($result, $array) use($key){
             isset($array[$key]) &&
