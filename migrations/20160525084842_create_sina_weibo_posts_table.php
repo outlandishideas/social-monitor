d283ed31ca381542e487fd922c33092ef06a9788
Matthew <mkendon@gmail.com>
2016-05-25 09:49:39 +0100

    added sina weibo posts

---
+++ migrations/20160525084842_create_sina_weibo_posts_table.php
@@ -0,0 +1,52 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class CreateSinaWeiboPostsTable extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * More information on this method is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-change-method
+     *
+     * Uncomment this method if you would like to use it.
+     *
+    public function change()
+    {
+    }
+    */
+    
+    /**
+     * Migrate Up.
+     */
+    public function up()
+    {
+    	$this->execute("CREATE TABLE `sina_weibo_posts` (
+		  `id` int(11) NOT NULL AUTO_INCREMENT,
+		  `remote_id` varchar(20) COLLATE utf8_bin NOT NULL,
+		  `text` mediumtext COLLATE utf8_bin NOT NULL,
+		  `presence_id` int(11) DEFAULT NULL COMMENT 'NULL when not posted by a known presence',
+		  `remote_user_id` varchar(20) COLLATE utf8_bin NOT NULL,
+		  `created_at` datetime NOT NULL,
+		  `picture_url` varchar(128) COLLATE utf8_bin DEFAULT NULL COMMENT 'NULL when no picture attached',
+		  `posted_by_presence` tinyint(1) NOT NULL DEFAULT '0' COMMENT 'has this post been posted by the presence itself',
+		  `included_retweet` varchar(20) COLLATE utf8_bin DEFAULT NULL COMMENT 'remote_id of included retweet. NULL when no included retweet with this post',
+		  `repost_count` int(11) unsigned NOT NULL DEFAULT '0',
+		  `comment_count` int(11) unsigned NOT NULL DEFAULT '0',
+		  `attitude_count` int(11) unsigned NOT NULL DEFAULT '0' COMMENT 'number of likes',
+		  PRIMARY KEY (`id`),
+		  UNIQUE KEY `remote_id` (`remote_id`),
+		  KEY `presence_id` (`presence_id`),
+		  KEY `included_retweet` (`included_retweet`)
+		) ENGINE=InnoDB AUTO_INCREMENT=3994 DEFAULT CHARSET=utf8 COLLATE=utf8_bin");
+    }
+
+    /**
+     * Migrate Down.
+     */
+    public function down()
+    {
+		$this->table('sina_weibo_posts')->drop();
+    }
+}
