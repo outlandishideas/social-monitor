f5f5d3179613fba18436dd9eb620a23fca031e06
Matthew <mkendon@gmail.com>
2016-05-25 09:38:44 +0100

    added kpi cache table migration

---
+++ migrations/20160525083733_create_kpi_cache_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class CreateKpiCacheTable extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * More information on this method is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-change-method
+     *
+     * Uncomment this method if you would like to use it.
+     *
+    public function change()
+    {
+    }
+    */
+
+    /**
+     * Migrate Up.
+     */
+    public function up()
+    {
+    	$this->execute("CREATE TABLE `kpi_cache` (
+		  `id` int(11) NOT NULL AUTO_INCREMENT,
+		  `presence_id` int(11) NOT NULL,
+		  `metric` varchar(50) NOT NULL,
+		  `start_date` date NOT NULL,
+		  `end_date` date NOT NULL,
+		  `value` float DEFAULT NULL,
+		  PRIMARY KEY (`id`),
+		  UNIQUE KEY `unique_index` (`presence_id`,`metric`,`start_date`,`end_date`),
+		  KEY `start_date` (`start_date`),
+		  KEY `end_date` (`end_date`)
+		) ENGINE=InnoDB AUTO_INCREMENT=11544167 DEFAULT CHARSET=utf8");
+    }
+
+    /**
+     * Migrate Down.
+     */
+    public function down()
+    {
+		$this->table('kpi_cache')->drop();
+    }
+}
