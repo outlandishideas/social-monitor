c3864da21a419d9ea990387755f15783ff27cc2a
Matthew <mkendon@gmail.com>
2016-05-25 16:20:41 +0100

    added phpdocs to new class

--- src/Validation/PasswordValidator.php
+++ src/Validation/PasswordValidator.php
@@ -1,22 +1,32 @@
 <?php
 namespace Outlandish\SocialMonitor\Validation;
 
+/**
+ * Validator for password fields
+ *
+ * @package Outlandish\SocialMonitor\Validation
+ */
 class PasswordValidator extends BaseValidator
 {
     protected $errorMessage = 'route.base.validation.wrong-password';
     private $user;
 
-    /** @var Model_User $user */
+	/**
+	 * @param Model_User $user The user the the the password validation is to be against
+	 */
     function __construct($user)
     {
         $this->user = $user;
     }
 
+	/**
+	 * Returns true if the sha1 of the $password string matches the $user->password_hash
+	 *
+	 * @param string $password the string to be validated
+	 * @return bool
+	 */
     public function isValid($password){
-        if($this->user && sha1(\Model_User::PASSWORD_SALT.$password) === $this->user->password_hash){
-            return true;
-        }else{
-            return false;
-        }
+        return $this->user &&
+			sha1(\Model_User::PASSWORD_SALT.$password) === $this->user->password_hash;
     }
 }
