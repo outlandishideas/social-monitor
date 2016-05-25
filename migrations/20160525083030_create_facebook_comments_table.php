8d8710b43622f2e90513bae506068af1b05d3841
Matthew <mkendon@gmail.com>
2016-05-25 09:31:38 +0100

    added facebook comments table

---
+++ migrations/20160525083030_create_facebook_comments_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class CreateFacebookCommentsTable extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * More information on this method is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-change-method
+     *
+     * Uncomment this method if you would like to use it.
+     *
+    public function change()
+    {
+    }
+    */
+
+    /**
+     * Migrate Up.
+     */
+    public function up()
+    {
+    	$this->execute("CREATE TABLE `facebook_comments` (
+		  `id` int(11) NOT NULL AUTO_INCREMENT,
+		  `comment_id` varchar(100) NOT NULL,
+		  `post_id` varchar(100) NOT NULL,
+		  `fromid` varchar(20) NOT NULL,
+		  `time` datetime NOT NULL,
+		  `text` varchar(500) NOT NULL,
+		  `likes` int(11) NOT NULL DEFAULT '0',
+		  PRIMARY KEY (`id`),
+		  UNIQUE KEY `comment_id` (`comment_id`),
+		  KEY `post_id` (`post_id`)
+		) ENGINE=InnoDB DEFAULT CHARSET=utf8");
+    }
+
+    /**
+     * Migrate Down.
+     */
+    public function down()
+    {
+		$this->table('facebook_comments')->drop();
+    }
+}
