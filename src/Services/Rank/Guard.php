ff78c71470d9db7fae2f6ec5fc2e6b99fe559995
Matthew Kendon <mkendon@gmail.com>
2016-07-06 15:31:29 +0100

    added some comments

--- src/Services/Rank/Guard.php
+++ src/Services/Rank/Guard.php
@@ -1,14 +1,13 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Matthew
- * Date: 06/07/2016
- * Time: 15:20
- */
 
 namespace Outlandish\SocialMonitor\Services\Rank;
 
-
+/**
+ * This guard stops the rendering of the rank on the presence and campaign view pages
+ *
+ * @package Outlandish\SocialMonitor\Services\Rank
+ * @author Matthew Kendon <mkendon@gmail.com>
+ */
 class Guard
 {
 	/**
@@ -16,15 +15,24 @@ class Guard
 	 */
 	private $userLevels;
 
+	/**
+	 * @param array $userLevels the user levels that should not be able to see the rank
+	 */
 	public function __construct(array $userLevels)
 	{
 		$this->userLevels = $userLevels;
 	}
-	
+
+	/**
+	 * Determines whether to the user (based on their user_level) should be able to see the rank
+	 *
+	 * @param \Model_User $user
+	 * @return bool
+	 */
 	public function canSee(\Model_User $user)
 	{
 		$level = $user ? $user->user_level : 1;
-		
+
 		return !in_array($level, $this->userLevels);
 	}
 }
